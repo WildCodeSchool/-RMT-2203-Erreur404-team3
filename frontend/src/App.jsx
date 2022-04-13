9b67f2b282d1b4fae5ce57dc843bc61c00de1953
Benoît Vandanjon <benoit@vandanjon.fr>
2022-04-13 15:56:37 +0200

    first common routes template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,36 @@
-import Home from "@pages/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Bidou from "./pages/DishesList";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    // <h1>pouet</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="disheslist">Dishes List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="disheslist" element={<Bidou />} />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
