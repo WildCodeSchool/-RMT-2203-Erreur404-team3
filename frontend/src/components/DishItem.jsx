8c655a581aec9d8e1bb902b5e66bfa0671ac64fa
Benoît Vandanjon <benoit@vandanjon.fr>
2022-04-14 16:03:30 +0200

    correct props types in eslintrc

--- frontend/src/components/DishItem.jsx
+++ frontend/src/components/DishItem.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import { Link } from "react-router-dom";
-// import Image from "@components/Image";
 
 function DishItem({ dish }) {
   return (
