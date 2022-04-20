1719ada6ec003f37b78c844a3c60b09dbb6aa4c0
Baasile <mathieu.basile@gmail.com>
2022-04-20 10:57:33 +0200

    add pages GEO and About.

--- frontend/src/pages/DishesInfos.jsx
+++ frontend/src/pages/DishesInfos.jsx
@@ -13,22 +13,3 @@ function DishesInfos() {
 }
 
 export default DishesInfos;
-
-//
-
-// import React from "react";
-// import dishes from './components/Data';
-// import axios from "axios";
-
-// function DishesInfos() {
-// return (
-//   <div>
-//      <h2>{dishes[0].itemName}</h2>
-//      <h2>{dishes[0].allergene}</h2>
-//      <h2>{dishes[0].description}</h2>
-//      <h2>Description</h2>
-//   </div>
-// );
-/// }
-
-// export default DishesInfos;
