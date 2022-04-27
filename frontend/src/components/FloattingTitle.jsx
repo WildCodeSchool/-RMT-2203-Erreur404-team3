51b54688128aed617b927e989a35417c77f049dc
GUILLAUME-GUERIT <guillaumeguerit@gmail.com>
2022-04-27 11:08:19 +0200

    resolved merge conflict

--- frontend/src/components/FloattingTitle.jsx
+++ frontend/src/components/FloattingTitle.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 
-export default function FloattingTitle({ props }) {
-  return <h1>{props.title}</h1>;
+export default function FloattingTitle({ title }) {
+  return <h1>{title}</h1>;
 }
