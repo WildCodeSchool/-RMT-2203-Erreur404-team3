2473c4c1086711889bb0b7ce63530a59c296c17d
AuroreA226 <89527415+AuroreA226@users.noreply.github.com>
2022-05-08 17:35:10 +0200

    modified donatingbutton component for about page

--- frontend/src/components/DonatingButton.jsx
+++ frontend/src/components/DonatingButton.jsx
@@ -1,71 +1,10 @@
-import { useState } from "react";
+import React from "react";
 
 export default function DonatingButton() {
-  const [isActive, setActive] = useState("false");
-
-  const handleToggle = () => {
-    setActive(!isActive);
-  };
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-xs-12 first-button">
-          <div className="button-1-contents-wrapper">
-            <button
-              className="btn btn-default donate-button"
-              type="button"
-              onClick={handleToggle}
-            >
-              Contribuer
-            </button>
-            <div
-              className={
-                isActive
-                  ? "preselected-donation-option option-1"
-                  : "preselected-donation-option option-1 moved"
-              }
-            >
-              0.50€
-            </div>
-            <div
-              className={
-                isActive
-                  ? "preselected-donation-option option-2"
-                  : "preselected-donation-option option-2 moved"
-              }
-            >
-              1€
-            </di>
-            <div
-              className={
-                isActive
-                  ? "preselected-donation-option option-3"
-                  : "preselected-donation-option option-3 moved"
-              }
-            >
-              2€
-            </div>
-            <div
-              className={
-                isActive
-                  ? "preselected-donation-option option-4"
-                  : "preselected-donation-option option-4 moved"
-              }
-            >
-              5€
-            </div>
-            <div
-              className={
-                isActive
-                  ? "preselected-donation-option option-other"
-                  : "preselected-donation-option option-other moved"
-              }
-            >
-              A votre bon ❤
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+    <a href="#" className="donate">
+      DONATE
+    </a>
   );
 }
