3bcb842e707345dae1173bf8991ee3d231a6bdab
Benoît Vandanjon <benoit@vandanjon.fr>
2022-05-10 17:18:07 +0200

    discarding all ESLINT errors despite the Map.jsx one as it is Google shitty code from old class

--- frontend/src/components/DonatingButton.jsx
+++ frontend/src/components/DonatingButton.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 
 export default function DonatingButton() {
-  return (
-    // eslint-disable-next-line jsx-a11y/anchor-is-valid
-    <a href="#" className="donate">
-      DONATE
-    </a>
-  );
+  return <div className="donate">DONATE</div>;
 }
--- frontend/src/components/Logos.jsx
+++ frontend/src/components/Logos.jsx
@@ -1,7 +1,3 @@
-/* eslint-disable import/extensions */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-
 function Logos(isFoodAdopted) {
   // La const pour gérér le changement de logo
   const img = isFoodAdopted
