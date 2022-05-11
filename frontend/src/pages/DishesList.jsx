fd0801eb7b0efff9207b39db7d74b85da8db5be4
GUILLAUME-GUERIT <guillaumeguerit@gmail.com>
2022-05-11 10:23:51 +0200

    modified Dishesinfos and DishesList to get data from db

--- frontend/src/pages/DishesList.jsx
+++ frontend/src/pages/DishesList.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import DishItem from "../components/DishItem";
-import dishes from "../datas/Dishes";
 
 function DishesList() {
+  const [dishes, setDishes] = React.useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/dishlist")
+      .then((res) => {
+        console.warn(res.data);
+        setDishes(res.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+    console.warn("e", dishes);
+  }, []);
+
   return (
     <div className="dishesList-container">
       <h2>Liste des plats</h2>
