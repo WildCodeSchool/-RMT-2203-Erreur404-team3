598919f3213eb5e5e546f60d9d766090840ef7d6
Benoît Vandanjon <benoit@vandanjon.fr>
2022-05-11 14:21:24 +0200

    on croise les doigts

--- frontend/src/components/DonatingButton.jsx
+++ frontend/src/components/DonatingButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function DonatingButton() {
   return (
     // eslint-disable-next-line jsx-a11y/anchor-is-valid
-    <a href="#" className="donate">
+    <a href="/pasdebol" className="donate">
       Contribuer💛
     </a>
   );
