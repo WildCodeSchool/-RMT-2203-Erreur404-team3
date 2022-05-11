fe363b9558d4ce5e6265b8d07d5915046fe1b203
AuroreA226 <89527415+AuroreA226@users.noreply.github.com>
2022-05-11 11:34:13 +0200

    create pagenotfound and modified app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import DisplayIngredient from "./pages/DisplayIngredient";
 import Ingredient from "./pages/Ingredient";
 import About from "./pages/About";
 import ValidationPage from "./pages/Validationpage";
+import PageNotFound from "./pages/PageNotFound";
 import "./App.scss";
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
         <Route path="/ingredient" element={<Ingredient />} />
         <Route path="/about" element={<About />} />
         <Route path="/validation" element={<ValidationPage />} />
+        <Route path="*" component={PageNotFound} />
       </Routes>
     </Router>
   );
